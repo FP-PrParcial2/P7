6a5ceb4a6a133382c7caed07f44bd59a77388cab
karen <karen@KarenZ>
2016-10-15 23:10:34 -0500

    problema 7 del proyecto

---
+++ src/ppproblema007/PPProblema007.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ppproblema007;
+import java.util.*;
+/**
+ *
+ * @author karen
+ */
+public class PPProblema007 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int km;
+       km=entradaDato();
+    }
+    public static int entradaDato(){
+        int km;
+        Scanner entrada= new Scanner(System.in);
+        System.out.println("Introduce los Km a convertir");
+        km=entrada.nextInt(); 
+        double res;
+        res= km * 0.277778;
+        System.out.println("El resultado es"+ res + "m/s");
+        return km;
+    }
+}
